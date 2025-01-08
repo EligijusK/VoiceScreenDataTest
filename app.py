ee3317152773c38266b9bdbf4c695f51a85ce42f
EligijusK <e.kiudys@gmail.com>
2025-01-08 16:38:31 +0200

    File merge works

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import os
 import random
 import time
@@ -14,16 +15,19 @@ def createDirectory(path):
 
 def file():
 
+
+
     arr = os.listdir("./voice")
     arrData= []
+
     for i in arr:
-        voice = wave.open("./voice/" + i, "rb")
+        voice = wave.open("./voice/" + i + "/oz.wav", "rb")
         sound = bytes()
         numberFrames = voice.getnframes()
         sound = voice.readframes(numberFrames)
         soundDataArray = np.frombuffer(sound, dtype=np.uint8)
         # myDump = json.dumps(args)
-        sessionID = random.randint(10000000, 99999999)
+        sessionID = i
 
         data = {
             'fileName': str(sessionID),
@@ -39,8 +43,49 @@ def file():
         arrData.append(jsonData)
 
     df = pd.json_normalize(arrData)
-    df.to_csv('employees.csv', index=False)
+    df.to_csv('employees.csv', mode='a', header=False)
+
+def merge_json_by_id():
+
+    df = pd.read_csv(r'./Database.csv')
+    df2 = pd.read_csv(r'./employees.csv')
+
+    database_df = pd.read_csv('./Database.csv')
+    employees_df = pd.read_csv('./employees.csv')
+
+    # Merge the two datasets on 'Patient no.' using an inner join
+    merged_inner = pd.merge(database_df, employees_df, on='Patient no.', how='inner')
+
+    print(employees_df)
+    columns_to_include = [
+        'Patient no.',
+        'AVQI',
+        'cpps',
+        'hnr',
+        'shPerc',
+        'shDB',
+        'timeTook',
+        'ltasSlope',
+        'ltasTreadTilt',
+        'timeTook',
+        'GFI 1',
+        'GFI 2',
+        'GFI 3',
+        'GFI 4',
+        'GFI Result'
+    ]
+
+    merged_inner_filtered = merged_inner[columns_to_include]
+    merged_inner_path = './merged_inner_filtered.csv'
+    merged_inner_filtered.to_csv(merged_inner_path, index=False)
+    print(f"Inner merge saved to: {merged_inner_path}")
 
 if __name__ == "__main__":
     file()
+    merge_json_by_id(
+        file1_path="./Database.csv",
+        file2_path="./employees.csv",
+        output_path="merged.json"
+    )
+
 
